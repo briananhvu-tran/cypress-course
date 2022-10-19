97661989994fb3bc05c39838d4b894d692b8407f
Brian Anhvu Tran <99tranbrian@gmail.com>
2022-10-18 19:26:22 -0700

    correct url test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -20,6 +20,17 @@ describe("home page", () => {
   context("Courses section", () => {
     it("Course: Testing Your First Next.js Application", () => {
       cy.getByData("course-0").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-your-first-application")
+    })
+
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-foundations")
+    })
+
+    it("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/cypress-fundamentals")
     })
   })
 })
